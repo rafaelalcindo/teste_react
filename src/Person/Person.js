aabfc8345955e82ec58977c4e4633fbd96f43af5
rafaelalcindo <rafael_alcindo@hotmail.com>
2018-06-25 15:14:41 -0300

    novas alterações

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Radium from 'radium';
+
+import './Person.css';
+
 const person = (props) => {
+    
     return (
-        <div>
+        <div className="Person"  >
             <p onClick={ props.click } >I'M a {props.name} and I am { props.age } Year old!</p>
             <p>{ props.children }</p>
-            <input type="text" onChange={props.changed}  />
+            <input type="text" onChange={props.changed} value={props.name} />
         </div>
     )
     
